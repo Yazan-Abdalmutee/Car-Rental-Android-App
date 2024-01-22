f24d3d013b35b74eab289066d244ef884f621123
Yazan-Almutte-CE <yazan.abd.almutte@gmail.com>
2024-01-22 14:13:55 +0200

    خذ لعلك تتق

--- app/src/main/java/com/example/finalproject/DataBase/DatabaseHelper.java
+++ app/src/main/java/com/example/finalproject/DataBase/DatabaseHelper.java
@@ -86,7 +86,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     static final String DB_NAME = "CAPITALCARS.DB";
     // database version
-    static final int DB_VERSION = 153;
+    static final int DB_VERSION = 155;
 
     public DatabaseHelper(Context context) {
         super(context, DB_NAME, null, DB_VERSION);
